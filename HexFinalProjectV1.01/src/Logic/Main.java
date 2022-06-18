555bd67a98db1c9ccdcf78954353d555b7474a19
CaptainHadar <48922927+CaptainHadar@users.noreply.github.com>
2022-06-18 19:48:03 +0300

    Add files via upload

---
+++ HexFinalProjectV1.01/src/Logic/Main.java
@@ -0,0 +1,17 @@
+package Logic;
+
+import GameGraphics.StartingGUI;
+public class Main {
+
+	
+
+	
+
+	
+	public static void main(String[] args){
+		StartingGUI game = new StartingGUI();
+		game.preview();
+		}
+
+	
+}
